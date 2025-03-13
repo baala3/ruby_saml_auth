e88527eaa1fbb8fec175749f230914696a3e053b
mfw-bala <kamandla.b@moneyforward.co.jp>
2025-03-13 15:53:59 +0900

    fire saml SSO request on button click

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 require_relative '../router'
+require 'zlib'
+require 'base64'
+require 'uri'
 
 # make sure to return an array from all routes
 Router.draw do
@@ -16,5 +19,22 @@ Router.draw do
   # Dynamic routes
   get('/acs') {
     [200, {'Content-Type' => 'text/html'}, ["successfully authenticated"]]
+  }
+
+  get('/auth/saml') {
+    # Read and process the template
+    template = File.read('xml/saml_request_template.xml')
+    saml_request = template
+      .gsub('{{HOST}}', ENV['HOST'])
+      .gsub('{{OKTA_DOMAIN}}', ENV['OKTA_DOMAIN'])
+      .gsub('{{OKTA_APP_PATH}}', ENV['OKTA_APP_PATH'])
+
+    # Deflate and encode
+    deflated_request = Zlib::Deflate.deflate(saml_request, Zlib::BEST_COMPRESSION)[2..-5]
+    base64_request = Base64.strict_encode64(deflated_request)
+    encoded_request = URI.encode_www_form_component(base64_request)
+
+    redirect_url = "https://#{ENV['OKTA_DOMAIN']}/#{ENV['OKTA_APP_PATH']}?SAMLRequest=#{encoded_request}"
+    [302, {'Location' => redirect_url}, []]
   }
 end
